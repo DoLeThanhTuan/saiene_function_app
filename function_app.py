6cad06fe58db11aa330a2288abb0e867324f493f
TZ - Phuong Tram <tz-phuongtram@techzen.vn>
2025-08-20 15:35:37 +0700

    add trigger

--- function_app.py
+++ function_app.py
@@ -1,11 +1,8 @@
 import azure.functions as func
 from api.main import app as main_app
 
-# 2. Khởi tạo Azure Function App
 app = func.FunctionApp()
 
-# 3. Đăng ký ứng dụng FastAPI với Azure Functions
-#    Route ở đây phải khớp với route prefix mà bạn định nghĩa.
 @app.route(route="{*route}", auth_level=func.AuthLevel.ANONYMOUS)
 async def healthcheck(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     """
