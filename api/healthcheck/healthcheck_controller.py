ef8fb5ba8e58b61fe1d60a855b3b9d1954b43be0
TZ - Phuong Tram <tz-phuongtram@techzen.vn>
2025-08-20 15:39:14 +0700

    add trigger

--- api/healthcheck/healthcheck_controller.py
+++ api/healthcheck/healthcheck_controller.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, Request
-from core.utils.response import response_success
 
 router = APIRouter()
 
@@ -10,17 +9,17 @@ def health_check_root(request: Request):
     """
     # Middleware đã xử lý xác thực, bạn có thể truy cập thông tin user ở đây
     # Ví dụ: user_email = request.state.user.email
-    return response_success("Health check is OK!")
+    return "Health check is OK!"
 
 @router.get("/details")
 def health_check_details():
     """
     Endpoint trả về thông tin chi tiết hơn.
     """
-    return response_success({
+    return {
         "status": "Healthy",
         "services": {
             "database": "Connected",
             "cache": "Connected"
         }
-    })
+    }
--- api/main.py
+++ api/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
 
-from core.utils.application import create_fastapi
 from api.healthcheck.healthcheck_controller import router as healthcheck_router
-from api.errorcheck.errorcheck_controller import router as errorcheck_router
 
 app = FastAPI()
 
 app.include_router(healthcheck_router,prefix="/api/healthcheck",tags=["Health Check"])
-app.include_router(errorcheck_router,prefix="/api/errorcheck",tags=["Error Check"])
