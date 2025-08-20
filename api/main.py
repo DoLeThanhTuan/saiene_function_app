d4aed19b8312358292c917d277c4ab92b98501e4
TZ - Phuong Tram <tz-phuongtram@techzen.vn>
2025-08-20 16:14:38 +0700

    add trigger

--- api/main.py
+++ api/main.py
@@ -1,24 +1,8 @@
 from fastapi import FastAPI
 
 from api.healthcheck.healthcheck_controller import router as healthcheck_router
-from configs.env import get_settings
-from exceptions.app_exception import AppException
-from exceptions.handler import app_exception_handler, system_exception_handler
-from exceptions.system_exception import SystemException
 
-settings = get_settings()
-fastapi_params = {
-        "title": settings.app_name,
-        "version": settings.api_version,
-    }
-app = FastAPI(**fastapi_params)
 
-__EXCEPTION_HANDLERS__ = [
-        (AppException, app_exception_handler),
-        (SystemException, system_exception_handler),
-    ]
-
-for exc, handler in __EXCEPTION_HANDLERS__:
-    app.add_exception_handler(exc, handler)
+app = FastAPI()
 
 app.include_router(healthcheck_router,prefix="/api/healthcheck",tags=["Health Check"])
