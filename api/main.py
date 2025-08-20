88b93c6db34b1d33c65ba18fb3a6a69d5118e0c4
TZ - Phuong Tram <tz-phuongtram@techzen.vn>
2025-08-20 16:01:02 +0700

    add trigger

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
 
 from api.healthcheck.healthcheck_controller import router as healthcheck_router
+from utils.application import create_fastapi
 
-app = FastAPI()
+app = create_fastapi()
 
 app.include_router(healthcheck_router,prefix="/api/healthcheck",tags=["Health Check"])
