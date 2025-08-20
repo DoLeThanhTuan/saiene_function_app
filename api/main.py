e17602c863c0325edf84185288d83a744fcfb4b9
TZ - Phuong Tram <tz-phuongtram@techzen.vn>
2025-08-20 16:34:49 +0700

    add trigger

--- api/main.py
+++ api/main.py
@@ -2,9 +2,6 @@ from fastapi import FastAPI
 
 from api.healthcheck.healthcheck_controller import router as healthcheck_router
 from configs.env import get_settings
-from exceptions.app_exception import AppException
-from exceptions.handler import app_exception_handler, system_exception_handler
-from exceptions.system_exception import SystemException
 
 settings = get_settings()
 
