333261222ae0907d3a3507d0c10bf5a2209667b0
TZ - Phuong Tram <tz-phuongtram@techzen.vn>
2025-08-20 17:04:22 +0700

    add trigger

--- api/errorcheck/errorcheck_controller.py
+++ api/errorcheck/errorcheck_controller.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from exceptions.app_exception import ConflictError
 
 router = APIRouter()
 
@@ -9,4 +10,4 @@ def health_check_error_test():
     Endpoint này cố tình gây ra lỗi để kiểm tra exception handler.
     """
     # Exception này sẽ được bắt bởi app_exception_handler mà bạn đã cấu hình
-    return "Error"
+    raise ConflictError()
